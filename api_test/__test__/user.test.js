084b9f83eaf7b1c1738b4222b31d592a82989cd9
Jett <newjett0617@gmail.com>
2019-05-19 18:02:49 +0800

    add api test for all users and single user

---
+++ api_test/__test__/user.test.js
@@ -0,0 +1,64 @@
+/**
+ * @jest-environment node
+ */
+
+const axios = require('axios');
+const baseURI = 'http://laravel-test.test';
+const {matchers} = require('jest-json-schema');
+
+expect.extend(matchers);
+
+it('fetch api/users/1 schema', async () => {
+    const schema = {
+        'type': 'object',
+        'required': [
+            'id', 'name', 'email', 'email_verified_at', 'created_at', 'updated_at'
+        ],
+        'properties': {
+            'id': {'type': 'integer',},
+            'name': {'type': 'string',},
+            'email': {'type': 'string',},
+            'email_verified_at': {'type': 'string',},
+            'created_at': {'type': 'string',},
+            'updated_at': {'type': 'string',}
+        }
+    };
+    expect((await fetchUsersByID(1)).data).toMatchSchema(schema);
+});
+
+test('fetch api/users/1 snapshot', async () => {
+    expect((await fetchUsersByID(1)).data).toMatchSnapshot();
+});
+
+it('fetch api/users schema', async () => {
+    const schema = {
+        'type': 'array',
+        'items': {
+            'type': 'object',
+            'required': [
+                'id', 'name', 'email', 'email_verified_at', 'created_at', 'updated_at'
+            ],
+            'properties': {
+                'id': {'type': 'integer',},
+                'name': {'type': 'string',},
+                'email': {'type': 'string',},
+                'email_verified_at': {'type': 'string',},
+                'created_at': {'type': 'string',},
+                'updated_at': {'type': 'string',}
+            }
+        }
+    };
+    expect((await fetchUsers()).data).toMatchSchema(schema);
+});
+
+test('fetch api/users snapshot', async () => {
+    expect((await fetchUsers()).data).toMatchSnapshot();
+});
+
+function fetchUsersByID(id) {
+    return axios.get(`${baseURI}/api/users/${id}`);
+}
+
+function fetchUsers() {
+    return axios.get(`${baseURI}/api/users`);
+}
